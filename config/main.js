b0ebf8d5a6f7077733594adda6c013926ebd303f
litheproxy <mynameisrohanandthisismyemail@gmail.com>
2024-12-10 14:14:48 -0500

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
 var sitename = "DecentGames"; // Change this to change the name of your website.
-var subtext = "V1"; // set the subtext
+var subtext = "Hi Wyatt"; // set the subtext
 
 // more settings in main.css
 
